c98e9927617e2e39470641f2552ecf4f93999e81
alekseyka221 <44611042+alekseyka221@users.noreply.github.com>
2020-11-11 15:05:59 +0200

    файл, с вызовом аввтоматических и пользовательских тестов

--- homelesson2/main.php
+++ homelesson2/main.php
@@ -3,13 +3,13 @@ require_once 'automaticTests.php';
 require_once 'usersTests.php';
 require_once 'readFromConsole.php';
 
-automaticTests();
+perfomAutomaticTests();
 
 echo PHP_EOL.'Хотители провести личные тесты?'.PHP_EOL.'Введите y/n'.PHP_EOL;
 $userChoice = readFromConsole();
 while($userChoice == 'y')
 {
-	usersTests();
+	perfomUsersTests();
 	echo 'Хотите провести тест еще?'.PHP_EOL.'Введите y/n'.PHP_EOL;
 	$userChoice = readFromConsole();
 }
