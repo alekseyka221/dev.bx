6aaccb0620b5fade02b62bebf7b434f4525e6a63
alekseyka221 <44611042+alekseyka221@users.noreply.github.com>
2020-11-11 19:13:51 +0200

    файл, с функцией пользовательских тестов

--- homelesson2/usersTests.php
+++ homelesson2/usersTests.php
@@ -8,6 +8,21 @@ function perfomUsersTests()
 
 	echo 'Ожидаемый результат: '.PHP_EOL;
 	$expectation = trim(fgets(STDIN));
+	switch ($expectation)
+	{
+		case is_numeric($expectation):
+			$expectation = +$expectation;
+			break;
+		case 'true':
+			$expectation = true;
+			break;
+		case 'false':
+			$expectation = false;
+			break;
+		default:
+			$expectation = (string)$expectation;
+			break;
+	}
 
 	if ($result == $expectation)
 	{
