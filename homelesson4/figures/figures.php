f408516d473960b1d667213aaebd31c3c8fc39b4
alekseyka221 <44611042+alekseyka221@users.noreply.github.com>
2020-11-29 19:53:07 +0200

    файл с классами ладья и ферзь

---
+++ homelesson4/figures/figures.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace AlexShah{
+
+	abstract class Figures
+	{
+		protected $line;
+		protected $column;
+		function __construct($a, $b)
+		{
+			$this->__set('column', $a);
+			$this->__set('line',$b);
+		}
+		function __set($name, $value)
+		{
+			if(is_numeric($value) && $value >= 1 && $value <= 8)
+			{
+				$this->$name = $value;
+			}
+		}
+		abstract public function checkAttack($column, $line);
+	}
+
+	class Rook extends Figures
+	{
+		function __construct($a, $b)
+		{
+			parent::__construct($a,$b);
+		}
+		function __set($a, $b)
+		{
+			parent::__set($a, $b);
+		}
+		public function checkAttack($column, $line) : string
+		{
+			if($line == $this->line || $column == $this->column)
+			{
+				return 'ДА';
+			}
+			else
+			{
+				return 'НЕТ';
+			}
+		}
+	}
+
+	class Queen extends Rook
+	{
+		function __construct($a, $b)
+		{
+			parent::__construct($a,$b);
+		}
+		public function checkAttack($column, $line) : string
+		{
+			if (parent::checkAttack($column, $line) == 'ДА')
+			{
+				return 'ДА';
+			}
+			for($curLine = $this->line, $curColumn = $this->column;
+			$curLine <= 8 && $curColumn <= 8; $curLine++, $curColumn++)
+			{
+				if ($line == $curLine && $column == $curColumn)
+				{
+					return 'ДА';
+				}
+			}
+			for($curLine = $this->line, $curColumn = $this->column;
+			$curLine >= 1 && $curColumn <= 8; $curLine--, $curColumn++)
+			{
+				if ($line == $curLine && $column == $curColumn)
+				{
+					return 'ДА';
+				}
+			}
+			for($curLine = $this->line, $curColumn = $this->column;
+			$curLine <= 8 && $curColumn >= 1; $curLine++, $curColumn--)
+			{
+				if ($line == $curLine && $column == $curColumn)
+				{
+					return 'ДА';
+				}
+			}
+			for($curLine = $this->line, $curColumn = $this->column;
+			$curLine >= 1 && $curColumn >= 1; $curLine--, $curColumn--)
+			{
+				if ($line == $curLine && $column == $curColumn)
+				{
+					return 'ДА';
+				}
+			}
+			return 'НЕТ';
+		}
+	}
+
+}
